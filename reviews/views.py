4cd8ea4456d20d9dd87e0a732b28743a5238e2eb
Gid Baerridge <gid.berridge@gmail.com>
2022-10-29 11:49:28 +0100

    all

--- reviews/views.py
+++ reviews/views.py
@@ -1,33 +1,37 @@
-from tkinter.tix import InputOnly
+from reviews.models import Review
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 from django.urls import is_valid_path
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+class ReviewView(CreateView):
+    model = Review
+    fields = "__all__"
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html", {
-        "has_error": False
-    })
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = "This Works!"
+        return context
+
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+
+class SingleReviewView(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
